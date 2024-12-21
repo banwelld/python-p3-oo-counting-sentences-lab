143d76b8c88a60fa698bb8ed5eb863e7213b3df4
Dave Banwell <dave@davebanwell.com>
2024-12-21 06:57:26 -0500

    all tests passing

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value: str = ""):
+    self.value = value
+    
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print("The value must be a string.")
+      
+  def is_sentence(self):
+    return True if self._value[-1] == "." else False
+  
+  def is_question(self):
+    return True if self._value[-1] == "?" else False
+  
+  def is_exclamation(self):
+    return True if self._value[-1] == "!" else False
+
+  def count_sentences(self):
+    stop_shouting = self._value.replace("!", ".")
+    stop_asking = stop_shouting.replace("?", ".")
+    sentences = stop_asking.split(".")
+    
+    sentence_count = 0
+    
+    for sentence in sentences:
+      if sentence:
+        sentence_count += 1
+      
+    return sentence_count
